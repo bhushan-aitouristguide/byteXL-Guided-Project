253911c52e07439633e7574586435d1548a23d2b
Bhushan Shewale <bhushanshevale3@gmail.com>
2024-11-06 23:57:07 +0530

    message

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      // colors: {
+      //   'custom-orange': '#FF6B35',
+      // }
+    },
+  },
+  plugins: [],
+}
+
